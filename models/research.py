1d8f720097365f92b6cdcf688d4b3e07ef2d81a9
beenish-majeed <beenishmajeed33@gmail.com>
2026-09-04 17:49:11 +0500

    feat: add research model

---
+++ models/research.py
@@ -0,0 +1,7 @@
+class Research:
+    
+    def __init__(self, topic, sources, summary, created_at):
+        self.topic = topic
+        self.sources = sources
+        self.summary = summary
+        self.created_at = created_at
