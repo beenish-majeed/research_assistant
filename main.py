ce31578ddea370272803d65877a25c7b68c9d7a0
beenish-majeed <beenishmajeed33@gmail.com>
2026-09-05 20:12:46 +0500

    feat: add saved research viewing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from models.research import Research
 from services.ai_service import ai_agent
-from utils.file_handler import saving
+from utils.file_handler import saving, load_research
 
 print("~" * 50)
 
@@ -8,15 +8,43 @@ print("         WELCOME TO RESEARCH ASSISTANT         ")
 
 print("~" * 50)
 
-question = input("\nEnter Topic: ").strip()
+while True:
+    print("\n1. Start Research")
+    print("2. View Saved Research")
+    print("3. Exit")
+    choice = input("\nEnter your choice: ").strip()
 
-while question == "":
-    print("Please enter a valid research topic.")
-    question = input("Enter Topic: ").strip()
+    if choice == "1":
+        question = input("\nEnter Topic: ").strip()
 
-r1 = Research(question) 
-result = ai_agent(question) 
-r1.summary = result["summary"] 
-r1.sources = result["sources"] 
-saving(r1) 
-print(r1.summary)
+        while question == "":
+            print("Please enter a valid research topic.")
+            question = input("Enter Topic: ").strip()
+
+        r1 = Research(question) 
+        result = ai_agent(question) 
+        if result is None: 
+            print("Research could not be generated. Please try again.") 
+        else: 
+            r1.summary = result["summary"] 
+            r1.sources = result["sources"] 
+            saving(r1) 
+            print(r1.summary)
+
+    elif choice == "2":
+        report = load_research("data/reports/research_1.json")
+
+        print("\nTopic:", report["topic"])
+        print("\nSummary:")
+        print(report["summary"])
+
+        print("\nSources:")
+        for source in report["sources"]:
+            print(source)
+
+    elif choice == "3":
+        print("Thanks for Visiting")
+        break
+
+    else:
+        print("Invalid choice")
--- utils/file_handler.py
+++ utils/file_handler.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 def saving(research):
     data = {
         "topic": research.topic,
@@ -8,16 +9,20 @@ def saving(research):
         "summary": research.summary,
         "sources": research.sources
     }
-    
+
     number = 1
 
     while os.path.exists(f"data/reports/research_{number}.json"):
         number += 1
 
-    filename = (f"data/reports/research_{number}.json")
+    filename = f"data/reports/research_{number}.json"
 
     with open(filename, "w") as file:
-        json.dump(data, file)
+        json.dump(data, file, indent=4)
 
-    print(data)
-    
+
+def load_research(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+
+    return data
